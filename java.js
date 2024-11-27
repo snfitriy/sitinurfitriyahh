94f4a6d8dda5c1899adc9871e364520ae3a5a4a6
snfitriy <sitinurfitriyah199@gmail.com>
2024-11-27 12:26:28 +0700

    Add files via upload
    Saya mengerjakan tugas dengan bantuan youtube dan w3 school

---
+++ java.js
@@ -0,0 +1,28 @@
+const slider = document.querySelector('.slider');
+
+function slideLeft() {
+  slider.scrollBy({
+    left: -320,
+    behavior: 'smooth'
+  });
+}
+
+function slideRight() {
+  slider.scrollBy({
+    left: 320,
+    behavior: 'smooth'
+  });
+}
+function slideLeft() {
+  slider.scrollBy({
+    left: -320,
+    behavior: 'smooth'
+  });
+}
+
+function slideRight() {
+  slider.scrollBy({
+    left: 320,
+    behavior: 'smooth'
+  });
+}
